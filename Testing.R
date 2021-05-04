967071b900fb4790fc3e86b5fa14f48ff9850728
Adonyo Emmanuel <=>
2021-05-04 09:29:46 +0100

    RmArkdown and Testing updated to include Rtools installation and use of markdown

--- Testing.R
+++ Testing.R
@@ -1,3 +1,17 @@
 print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
+find.package("devtools")
+
+#install.packages("tinytex")
+tinytex::install_tinytex()
+library(devtools)
+find_rtools()
+writeLines('PATH="${RTOOLS40_HOME}\\usr\\bin;${PATH}"', con = "~/.Renviron")
+
+Sys.which("make")
+Sys.getenv('PATH')
+## "C:\\rtools40\\usr\\bin\\make.exe"
+
+Sys.which("ls.exe")
+Sys.which("gcc.exe")
